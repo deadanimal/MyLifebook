5de2ba05e6599dafc7dc6a3a9ba69c831cf11422
Afeez Aziz <afeezaziz@gmail.com>
2023-05-04 13:30:21 +0800

    1

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -10,6 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     
-    Route::delete('/logout', [AuthController::class, 'logout']);   
+    Route::post('/logout', [AuthController::class, 'logout']);   
     
 });
