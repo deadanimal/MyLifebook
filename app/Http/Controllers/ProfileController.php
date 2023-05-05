e4675bdd50eacfb04c2ded398727c8079f800f52
Afeez Aziz <afeezaziz@gmail.com>
2023-05-05 10:52:02 +0800

    123

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -42,7 +42,7 @@ class ProfileController extends Controller
         $token = PersonalAccessToken::findToken($tokenQuery);
         $user = $token->tokenable;
         $user->createOrGetStripeCustomer();
-        return $request->user()->redirectToBillingPortal(route('dashboard'));        
+        return $user->redirectToBillingPortal(route('dashboard'));        
     }        
 
     public function create_profile(Request $request) {
