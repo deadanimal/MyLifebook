a5f7bbd95590084c48ff1f43fff5e30c50344716
Afeez Aziz <afeezaziz@gmail.com>
2023-05-10 22:12:27 +0800

    3

--- resources/views/static/home.blade.php
+++ resources/views/static/home.blade.php
@@ -6,7 +6,7 @@
         <header class="absolute inset-x-0 top-0 z-50">
             <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
               <div class="flex lg:flex-1">
-                <a href="#" class="-m-1.5 p-1.5">
+                <a href="/" class="-m-1.5 p-1.5">
                   <span class="sr-only">Your Company</span>
                   <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
                 </a>
@@ -20,13 +20,13 @@
                 </button>
               </div>
               <div class="hidden lg:flex lg:gap-x-12">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
+                {{-- <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a> --}}
               </div>
               <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+                <a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
               </div>
             </nav>
             <!-- Mobile menu, show/hide based on menu open state. -->
@@ -35,7 +35,7 @@
               <div class="fixed inset-0 z-50"></div>
               <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                  <a href="#" class="-m-1.5 p-1.5">
+                  <a href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
                     <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
                   </a>
