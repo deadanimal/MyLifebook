6b0e380fe3e943856848490fcb53d509ffe3375b
Afeez Aziz <afeezaziz@gmail.com>
2023-05-10 21:42:21 +0800

    123

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -24,7 +24,7 @@
                     <div class="flex items-center justify-between">
                         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                         <div class="text-sm">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
+                            <a href="/forgot-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
                                 password?</a>
                         </div>
                     </div>
@@ -43,8 +43,7 @@
 
             <p class="mt-10 text-center text-sm text-gray-500">
                 Not a member?
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free
-                    trial</a>
+                <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register an account</a>
             </p>
         </div>
     </div>
