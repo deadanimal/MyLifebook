54a14b73603384d859be7ce967cd0ce87bd88d10
Afeez Aziz <afeezaziz@gmail.com>
2023-05-11 19:02:16 +0800

    1

--- resources/views/app/rewinds.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    @forelse($rewinds as $rewind)
-        {{ $rewind }} <br />
-    @empty
-        No rewind Asked Yet
-    @endforelse
-@endsection
