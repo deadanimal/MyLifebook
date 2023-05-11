3f96de2f43d666820e977ad0a708da908af9577d
Afeez Aziz <afeezaziz@gmail.com>
2023-05-11 19:04:06 +0800

    123

---
+++ resources/views/app/prompts.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="py-10">
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Record</h1>
+            </div>
+        </header>
+        <main class="py-6">
+
+         
+        </main>
+    </div>
+@endsection
