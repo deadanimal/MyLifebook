ce68d3d46a9e40421f5fe6b61b719b2a3c7e571a
Afeez Aziz <afeezaziz@gmail.com>
2023-05-12 00:47:42 +0800

    1

--- resources/views/static/terms.blade.php
+++ resources/views/static/terms.blade.php
@@ -9,107 +9,66 @@
 
             <div class="mt-16 max-w-2xl">
                 <p class="mt-6">
-                    Welcome to My Memoir! These terms and conditions outline the rules and regulations for the use of DCB
-                    Platform Limited's Website, located at
-                    https://memoir.my.
+                    Welcome to My Memoir! These terms and conditions outline the rules and regulations for the use of DCB Platform Limited's Website, located at https://memoir.my.
                 </p>
                 <p class="mt-6">
-                    By accessing this website, we assume you accept these terms and conditions. Do not continue to use My
-                    Memoir if you do
-                    not agree to take all of the terms and conditions stated on this page.
+                    By accessing this website, we assume you accept these terms and conditions. Do not continue to use My Memoir if you do not agree to take all of the terms and conditions stated on this page.
                 </p>
                 <p class="mt-6">
-                    Cookies:
-                    The website uses cookies to help personalize your online experience. By accessing My Memoir, you agreed
-                    to use the
-                    required cookies.
+                    Cookies:<br/>
+                    The website uses cookies to help personalize your online experience. By accessing My Memoir, you agreed to use the required cookies.
                 </p>
                 <p class="mt-6">
-                    A cookie is a text file that is placed on your hard disk by a web page server. Cookies cannot be used to
-                    run programs or
-                    deliver viruses to your computer. Cookies are uniquely assigned to you and can only be read by a web
-                    server in the
-                    domain that issued the cookie to you.
+                    A cookie is a text file that is placed on your hard disk by a web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you and can only be read by a web server in the domain that issued the cookie to you.
                 </p>
                 <p class="mt-6">
-                    We may use cookies to collect, store, and track information for statistical or marketing purposes to
-                    operate our
-                    website. You have the ability to accept or decline optional Cookies. There are some required Cookies
-                    that are necessary
-                    for the operation of our website. These cookies do not require your consent as they always work. Please
-                    keep in mind
-                    that by accepting required Cookies, you also accept third-party Cookies, which might be used via
-                    third-party provided
-                    services if you use such services on our website, for example, a video display window provided by third
-                    parties and
-                    integrated into our website.
-
-                    License:
-                    Unless otherwise stated, DCB Platform Limited and/or its licensors own the intellectual property rights
-                    for all material
-                    on My Memoir. All intellectual property rights are reserved. You may access this from My Memoir for your
-                    own personal
-                    use subjected to restrictions set in these terms and conditions.
-
+                    We may use cookies to collect, store, and track information for statistical or marketing purposes to operate our website. You have the ability to accept or decline optional Cookies. There are some required Cookies that are necessary for the operation of our website. These cookies do not require your consent as they always work. Please keep in mind that by accepting required Cookies, you also accept third-party Cookies, which might be used via third-party provided services if you use such services on our website, for example, a video display window provided by third parties and integrated into our website.
+                </p>
+                <p class="mt-6">
+                    License:<br/>
+                    Unless otherwise stated, DCB Platform Limited and/or its licensors own the intellectual property rights for all material on My Memoir. All intellectual property rights are reserved. You may access this from My Memoir for your own personal use subjected to restrictions set in these terms and conditions.
+                </p>
+                <p class="mt-6">
                     You must not:
 
-                    Copy or republish material from My Memoir
-                    Sell, rent, or sub-license material from My Memoir
-                    Reproduce, duplicate or copy material from My Memoir
-                    Redistribute content from My Memoir
-                    This Agreement shall begin on the date hereof.
+                    <br/>Copy or republish material from My Memoir
+                    <br/>Sell, rent, or sub-license material from My Memoir
+                    <br/>Reproduce, duplicate or copy material from My Memoir
+                    <br/>Redistribute content from My Memoir
+                    <br/>This Agreement shall begin on the date hereof.
 
-                    Parts of this website offer users an opportunity to post and exchange opinions and information in
-                    certain areas of the
-                    website. DCB Platform Limited does not filter, edit, publish or review Comments before their presence on
-                    the website.
-                    Comments do not reflect the views and opinions of DCB Platform Limited, its agents, and/or affiliates.
-                    Comments reflect
-                    the views and opinions of the person who posts their views and opinions. To the extent permitted by
-                    applicable laws, DCB
-                    Platform Limited shall not be liable for the Comments or any liability, damages, or expenses caused
-                    and/or suffered as a
-                    result of any use of and/or posting of and/or appearance of the Comments on this website.
-
+                </p>
+                <p class="mt-6">                    
+                    Parts of this website offer users an opportunity to post and exchange opinions and information in certain areas of the website. DCB Platform Limited does not filter, edit, publish or review Comments before their presence on the website. Comments do not reflect the views and opinions of DCB Platform Limited, its agents, and/or affiliates. Comments reflect the views and opinions of the person who posts their views and opinions. To the extent permitted by applicable laws, DCB Platform Limited shall not be liable for the Comments or any liability, damages, or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
+                </p>
+                <p class="mt-6">
                     DCB Platform Limited reserves the right to monitor all Comments and remove any Comments that can be
                     considered
                     inappropriate, offensive, or causes breach of these Terms and Conditions.
-
+                </p>
+                <p class="mt-6">
                     You warrant and represent that:
 
-                    You are entitled to post the Comments on our website and have all necessary licenses and consents to do
-                    so;
-                    The Comments do not invade any intellectual property right, including without limitation copyright,
-                    patent, or trademark
-                    of any third party;
-                    The Comments do not contain any defamatory, libelous, offensive, indecent, or otherwise unlawful
-                    material, which is an
-                    invasion of privacy.
-                    The Comments will not be used to solicit or promote business or custom or present commercial activities
-                    or unlawful
-                    activity.
-                    You hereby grant DCB Platform Limited a non-exclusive license to use, reproduce, edit and authorize
-                    others to use,
-                    reproduce and edit any of your Comments in any and all forms, formats, or media.
+                    You are entitled to post the Comments on our website and have all necessary licenses and consents to do so; <br/>
+                    The Comments do not invade any intellectual property right, including without limitation copyright, patent, or trademark of any third party; <br/>
+                    The Comments do not contain any defamatory, libelous, offensive, indecent, or otherwise unlawful material, which is an invasion of privacy. The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity. You hereby grant DCB Platform Limited a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats, or media. <br/>
+                </p>
+                <p class="mt-6">
+                    Hyperlinking to our Content: <br/>
+                    
+                    The following organizations may link to our Website without prior written approval: <br/>
 
-                    Hyperlinking to our Content:
-                    The following organizations may link to our Website without prior written approval:
-
-                    Government agencies;
-                    Search engines;
-                    News organizations;
-                    Online directory distributors may link to our Website in the same manner as they hyperlink to the
-                    Websites of other
-                    listed businesses; and
-                    System-wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls,
-                    and charity
-                    fundraising groups which may not hyperlink to our Web site.
-                    These organizations may link to our home page, to publications, or to other Website information so long
-                    as the link: (a)
-                    is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement, or approval of the
-                    linking party and
-                    its products and/or services; and (c) fits within the context of the linking party's site.
-
+                    Government agencies; <br/>
+                    Search engines; <br/>
+                    News organizations; <br/>
+                    Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and <br/>
+                    System-wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site. <br/>
+                    These organizations may link to our home page, to publications, or to other Website information so long as the link: <br/>
+                    (a) is not in any way deceptive; <br/>
+                    (b) does not falsely imply sponsorship, endorsement, or approval of the linking party and its products and/or services; and <br/>
+                    (c) fits within the context of the linking party's site. <br/>
+                </p>
+                <p class="mt-6">
                     We may consider and approve other link requests from the following types of organizations:
 
                     commonly-known consumer and/or business information sources;
@@ -126,13 +85,15 @@
                     us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of DCB Platform
                     Limited; and (d)
                     the link is in the context of general resource information.
-
+                </p>
+                <p class="mt-6">
                     These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b)
                     does not falsely
                     imply sponsorship, endorsement, or approval of the linking party and its products or services; and (c)
                     fits within the
                     context of the linking party's site.
-
+                </p>
+                <p class="mt-6">
                     If you are one of the organizations listed in paragraph 2 above and are interested in linking to our
                     website, you must
                     inform us by sending an e-mail to DCB Platform Limited. Please include your name, your organization
@@ -140,7 +101,8 @@
                     information as well as the URL of your site, a list of any URLs from which you intend to link to our
                     Website, and a list
                     of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.
-
+                </p>
+                <p class="mt-6">
                     Approved organizations may hyperlink to our Website as follows:
 
                     By use of our corporate name; or
@@ -150,7 +112,8 @@
                     the linking party's site.
                     No use of DCB Platform Limited's logo or other artwork will be allowed for linking absent a trademark
                     license agreement.
-
+                </p>
+                <p class="mt-6">
                     Content Liability:
                     We shall not be held responsible for any content that appears on your Website. You agree to protect and
                     defend us
@@ -159,7 +122,8 @@
                     libelous, obscene, or criminal, or which infringes, otherwise violates, or advocates the infringement or
                     other violation
                     of, any third party rights.
-
+                </p>
+                <p class="mt-6">
                     Reservation of Rights:
                     We reserve the right to request that you remove all links or any particular link to our Website. You
                     approve to
@@ -168,36 +132,31 @@
                     and its linking policy at any time. By continuously linking to our Website, you agree to be bound to and
                     follow these
                     linking terms and conditions.
-
+                </p>
+                <p class="mt-6">
                     Removal of links from our website:
                     If you find any link on our Website that is offensive for any reason, you are free to contact and inform
                     us at any
                     moment. We will consider requests to remove links, but we are not obligated to or so or to respond to
                     you directly.
+                </p>
+                <p class="mt-6">
+                    We do not ensure that the information on this website is correct. We do not warrant its completeness or accuracy, nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+                </p>
+                <p class="mt-6">
+                    Disclaimer: <br/>
+                    To the maximum extent permitted by applicable law, we exclude all representations, warranties, and conditions relating to our website and the use of this website. Nothing in this disclaimer will:
 
-                    We do not ensure that the information on this website is correct. We do not warrant its completeness or
-                    accuracy, nor do
-                    we promise to ensure that the website remains available or that the material on the website is kept up
-                    to date.
-
-                    Disclaimer:
-                    To the maximum extent permitted by applicable law, we exclude all representations, warranties, and
-                    conditions relating
-                    to our website and the use of this website. Nothing in this disclaimer will:
-
-                    limit or exclude our or your liability for death or personal injury;
-                    limit or exclude our or your liability for fraud or fraudulent misrepresentation;
-                    limit any of our or your liabilities in any way that is not permitted under applicable law; or
-                    exclude any of our or your liabilities that may not be excluded under applicable law.
-                    The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a)
-                    are subject to
-                    the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including
-                    liabilities arising in
-                    contract, in tort, and for breach of statutory duty.
-
-                    As long as the website and the information and services on the website are provided free of charge, we
-                    will not be
-                    liable for any loss or damage of any nature.
+                    limit or exclude our or your liability for death or personal injury; <br/>
+                    limit or exclude our or your liability for fraud or fraudulent misrepresentation; <br/>
+                    limit any of our or your liabilities in any way that is not permitted under applicable law; or <br/>
+                    exclude any of our or your liabilities that may not be excluded under applicable law.<br/>
+                    The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: <br>
+                    (a) are subject to the preceding paragraph; and <br/>
+                    (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort, and for breach of statutory duty.
+                </p>
+                <p class="mt-6">
+                    As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.
                 </p>
 
 
